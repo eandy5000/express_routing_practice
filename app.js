4469edf74e3bfcbb5e677e5fbee64302ad498c79
Andy Eccleston <eandy5000@yahoo.com>
2016-08-30 09:27:36 -0500

    added simple listerner with express

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.set('port', 5000);
 
-console.log("works");
+// app.listen(app.get('port'), function(){
+//     console.log("listening on port " + app.get('port'));
+// });
+
+server = app.listen(app.get('port'), function(){
+    var port = server.address().port;
+    console.log('listening on port ' + port);
+});
+
+
