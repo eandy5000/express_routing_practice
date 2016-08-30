7d8742accd173d2949de406afa9b87ae72e1919b
Andy Eccleston <eandy5000@yahoo.com>
2016-08-30 10:24:05 -0500

    basic express routes and serving files

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', 5000);
 
-// app.listen(app.get('port'), function(){
-//     console.log("listening on port " + app.get('port'));
-// });
+app.get('/', function(req, res){
+    console.log('get the homepage');
+    res
+        .status(200)
+        .send("home");
+});
+
+app.get('/file', function(req, res){
+    console.log('getting file');
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'app.js'));
+});
 
 server = app.listen(app.get('port'), function(){
     var port = server.address().port;
