2e54b2cd2ecb840d40492d62ef341cc3d309e84d
Andy Eccleston <eandy5000@yahoo.com>
2016-08-31 11:10:11 -0500

    added data folder with json file, now controller is pulling data from this file

--- api/controllers/hotels.controller.js
+++ api/controllers/hotels.controller.js
@@ -1,7 +1,9 @@
+var hotelData = require('../data/data.json');
+
 
 module.exports.getAllHotels = function(req, res){
     console.log("getting file");
     res
         .status(200)
-        .send("working");
+        .json(hotelData);
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var ctrlHotels = require('../controllers/hotels.controller.js');
 
 router
-    .route('/file')
+    .route('/hotels')
     .get(ctrlHotels.getAllHotels);
 router
     .route('/test')
